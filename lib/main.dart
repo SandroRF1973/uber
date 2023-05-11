fe6948399afe1b36fdec4eb7a345b4e38aa84661
SandroRF <sandrorf1973@gmail.com>
2023-05-11 10:51:44 -0300

    Aula - 209

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:uber/telas/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Uber",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
