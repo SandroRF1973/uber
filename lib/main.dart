23a2f5b8b6f6199990aa16392659cfa9f53816a3
Sandro Ferreira <sandrorf1973@gmail.com>
2023-05-12 20:39:47 -0300

    Aula - 211

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:uber/telas/home.dart';
 
+final ThemeData theme = ThemeData();
+
+final ThemeData temaPadrao = ThemeData().copyWith(
+    colorScheme: theme.colorScheme.copyWith(
+        primary: const Color(0xff37474f), secondary: const Color(0xff546e7a)));
+
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     title: "Uber",
-    home: Home(),
+    home: const Home(),
+    theme: temaPadrao,
     debugShowCheckedModeBanner: false,
   ));
 }
--- lib/telas/cadastro.dart
+++ lib/telas/cadastro.dart
@@ -1,42 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:uber/telas/cadastro.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class Cadastro extends StatefulWidget {
+  const Cadastro({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<Cadastro> createState() => _CadastroState();
 }
 
-class _HomeState extends State<Home> {
+class _CadastroState extends State<Cadastro> {
+  final TextEditingController _controllerNome = TextEditingController();
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerSenha = TextEditingController();
+  bool _tipoUsuarioPassageiro = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cadastro"),
+      ),
       body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("imagens/fundo.png"), fit: BoxFit.cover),
-        ),
         padding: const EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 32),
-                  child: Image.asset(
-                    "imagens/logo.png",
-                    width: 200,
-                    height: 150,
-                  ),
+                TextField(
+                  controller: _controllerNome,
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Nome completo",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6))),
                 ),
                 TextField(
                   controller: _controllerEmail,
-                  autofocus: true,
                   keyboardType: TextInputType.emailAddress,
                   style: const TextStyle(fontSize: 20),
                   decoration: InputDecoration(
@@ -61,6 +65,22 @@ class _HomeState extends State<Home> {
                           borderRadius: BorderRadius.circular(6))),
                 ),
                 Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      const Text("Passageiro"),
+                      Switch(
+                          value: _tipoUsuarioPassageiro,
+                          onChanged: (bool valor) {
+                            setState(() {
+                              _tipoUsuarioPassageiro = valor;
+                            });
+                          }),
+                      const Text("Motorista")
+                    ],
+                  ),
+                ),
+                Padding(
                   padding: const EdgeInsets.only(top: 16, bottom: 10),
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
@@ -68,23 +88,9 @@ class _HomeState extends State<Home> {
                         padding: const EdgeInsets.fromLTRB(32, 16, 32, 16)),
                     onPressed: () {},
                     child: const Text(
-                      "Entrar",
+                      "Cadastrar",
                       style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
-                  ),
-                ),
-                Center(
-                  child: GestureDetector(
-                    child: const Text(
-                      "Não tem conta? cadastre-se!",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Cadastro()));
-                    },
                   ),
                 ),
                 const Padding(
