6869183cc2b7516b2a53d3dfb37e4ec44025b838
Sandro Ferreira <sandrorf1973@gmail.com>
2023-05-21 19:22:09 -0300

    Aula - 218

--- lib/telas/painel_passageiro.dart
+++ lib/telas/painel_passageiro.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:geolocator/geolocator.dart';
+import 'dart:io';
 
 class PainelPassageiro extends StatefulWidget {
   const PainelPassageiro({super.key});
@@ -10,6 +14,10 @@ class PainelPassageiro extends StatefulWidget {
 
 class _PainelPassageiroState extends State<PainelPassageiro> {
   List<String> itensMenu = ["Configurações", "Deslogar"];
+
+  final Completer<GoogleMapController> _controller = Completer();
+  CameraPosition _posicaoCamera =
+      const CameraPosition(target: LatLng(-23.563999, -46.653256));
 
   _deslogarUsuario() async {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -28,6 +36,56 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
       case "Configurações":
         break;
     }
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  _adicionarListenerLocalizacao() {
+    var locationOptions = const LocationSettings(
+        accuracy: LocationAccuracy.high, distanceFilter: 10);
+
+    Geolocator.getPositionStream(locationSettings: locationOptions)
+        .listen((Position position) {
+      _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude), zoom: 19);
+
+      _movimentarCamera(_posicaoCamera);
+
+      // setState(() {
+      //   _marcadores.add(marcadorUsuario);
+      //   _posicaoCamera = CameraPosition(
+      //       target: LatLng(position.latitude, position.longitude), zoom: 17);
+      //   _movimentarCamera();
+      // });
+    });
+  }
+
+  _recuperarUltimaLocalizacaoConhecida() async {
+    Position? position = await Geolocator.getLastKnownPosition();
+
+    setState(() {
+      if (position != null) {
+        _posicaoCamera = CameraPosition(
+            target: LatLng(position.latitude, position.longitude), zoom: 19);
+
+        _movimentarCamera(_posicaoCamera);
+      }
+    });
+  }
+
+  _movimentarCamera(CameraPosition cameraPosition) async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarUltimaLocalizacaoConhecida();
+    _adicionarListenerLocalizacao();
   }
 
   @override
@@ -49,7 +107,105 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
           )
         ],
       ),
-      body: Container(),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _posicaoCamera,
+            onMapCreated: _onMapCreated,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false,
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white),
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                      // ignore: sized_box_for_whitespace
+                      icon: Container(
+                        margin: const EdgeInsets.only(left: 20, bottom: 10),
+                        width: 10,
+                        height: 10,
+                        child: const Icon(
+                          Icons.location_on,
+                          color: Colors.green,
+                        ),
+                      ),
+                      hintText: "Meu local",
+                      border: InputBorder.none,
+                      contentPadding:
+                          const EdgeInsets.only(left: 15, top: 16, bottom: 10)),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 55,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white),
+                child: TextField(
+                  decoration: InputDecoration(
+                      // ignore: sized_box_for_whitespace
+                      icon: Container(
+                        margin: const EdgeInsets.only(left: 20, bottom: 10),
+                        width: 10,
+                        height: 10,
+                        child: const Icon(
+                          Icons.local_taxi,
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "Digite o destino",
+                      border: InputBorder.none,
+                      contentPadding:
+                          const EdgeInsets.only(left: 15, top: 16, bottom: 10)),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+              right: 0,
+              left: 0,
+              bottom: 0,
+              child: Padding(
+                padding: Platform.isIOS
+                    ? const EdgeInsets.fromLTRB(20, 10, 20, 25)
+                    : const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff1ebbd8),
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16)),
+                  onPressed: () {
+                    //_validarCampos();
+                  },
+                  child: const Text(
+                    "Chamar Uber",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
